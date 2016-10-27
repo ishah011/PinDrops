37a87d20a820ecd9a4bba99b0024e7b1da60f550
root <root@fa16-cs411-29.cs.illinois.edu>
2016-10-27 01:15:11 -0500

    pindrops.py now renders index.html and allows it to display the query

--- pindrops.py
+++ pindrops.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask import render_template
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -9,17 +10,17 @@ app.config['MYSQL_DB'] = 'imdb'
 app.config['MYSQL_HOST'] = 'fa16-cs411-29.cs.illinois.edu'
 
 @app.route('/')
-def users(): 
+def index(store=None): 
     current = ""
     cur = mysql.connection.cursor()
     to_exec = "SELECT * FROM Actors WHERE name='Depp, Johnny'"
     cur.execute("""SELECT * FROM Actors WHERE name='Depp, Johnny'""")
     rv = cur.fetchall()
     list(rv)
-    store=[]
+    store = []
     for i in rv[0]:
         store.append(str(i))
-    return str(",".join(store))+'\n Query: '+to_exec
-
+    #return str(",".join(store))+'\n Query: '+to_exec
+    return render_template('index.html',store=store)
 if __name__ == '__main__':
     app.run(debug=True)
