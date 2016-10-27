98291b5a59af342cfcf4e51676d4e6e9893ed1d4
root <root@fa16-cs411-29.cs.illinois.edu>
2016-10-27 01:03:04 -0500

    __init__.py test works with Flask

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
 
+app = Flask(__name__)
+
 @app.route('/index/')
-def index:
+def index(rv=None):
 	rv = [1,2,3,4,5,6,7,8,9,10]
-	return render_template('index.html')
+	return render_template('index.html',rv=rv)
--- pindrops.py
+++ pindrops.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 mysql = MySQL(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'cs411fa2016'
-app.config['MYSQL_DB'] = 'imdbIn2'
+app.config['MYSQL_DB'] = 'imdb'
 app.config['MYSQL_HOST'] = 'fa16-cs411-29.cs.illinois.edu'
 
 @app.route('/')
-def users():
+def users(): 
+    current = ""
     cur = mysql.connection.cursor()
-    cur.execute("""SELECT * FROM name WHERE name='Depp, Johnny'""")
-    rv = cur.fetchone()
-    name = rv[1]
-    return str(name)
+    to_exec = "SELECT * FROM Actors WHERE name='Depp, Johnny'"
+    cur.execute("""SELECT * FROM Actors WHERE name='Depp, Johnny'""")
+    rv = cur.fetchall()
+    list(rv)
+    store=[]
+    for i in rv[0]:
+        store.append(str(i))
+    return str(",".join(store))+'\n Query: '+to_exec
 
 if __name__ == '__main__':
     app.run(debug=True)
