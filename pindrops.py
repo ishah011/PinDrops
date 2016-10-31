bbf875568ced864cc08afc8596ca0ca21f4b17f2
root <root@fa16-cs411-29.cs.illinois.edu>
2016-10-30 20:12:41 -0500

    added routing functions to pindrops.py, works for home page. need to fix login page

--- pindrops.py
+++ pindrops.py
@@ -22,5 +22,37 @@ def index(store=None):
         store.append(str(i))
     #return str(",".join(store))+'\n Query: '+to_exec
     return render_template('index.html',store=store)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('INSERT INTO Users(email, password, firstName, lastName) values (?, ?, ?, ?)',
+                 [request.form['Email'], request.form['Password'], request.form['Fname'], request.form['Lname']])
+    db.commit()
+    flash('Account created')
+    return redirect('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['Email'] != app.config['EMAIL']:
+            error = 'Invalid email'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
