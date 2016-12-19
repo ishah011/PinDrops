87df8d5b92d41b0bbf15ff0cbc02f6a617918614
Isheeta Shah <isheeta_s@yahoo.com>
2016-12-18 18:37:45 -0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-
-@app.route('/index/')
-def index(rv=None):
-	rv = [1,2,3,4,5,6,7,8,9,10]
-	return render_template('index.html',rv=rv)
